70e576a62dd0e76f6c2690036020aab4ace2416d
Henry Ashcroft <117559890+ash-hensday@users.noreply.github.com>
2025-01-23 12:28:32 -0800

    Add files via upload

--- Model.py
+++ Model.py
@@ -1,18 +1,23 @@
-from transformers import AlbertTokenizer, AlbertForSequenceClassification, Trainer, TrainingArguments
+from transformers import AlbertTokenizer, AlbertForSequenceClassification, Trainer, TrainingArguments, DataCollatorWithPadding
 import torch
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 
 class LLMModel:
-    def __init__(self, model_name='albert-base-v2', num_labels=2):
+    def __init__(self, model_name='albert-base-v2', num_labels=2, class_weights=None):
         self.tokenizer = AlbertTokenizer.from_pretrained(model_name)
-        self.model = AlbertForSequenceClassification.from_pretrained(model_name, num_labels=num_labels)
+        self.model = AlbertForSequenceClassification.from_pretrained(model_name, num_labels=num_labels, 
+                                                                     problem_type="single_label_classification",
+                                                                     hidden_dropout_prob=0.1)
+        if class_weights is not None:
+            self.model.loss_fct = torch.nn.CrossEntropyLoss(weight=torch.tensor(class_weights))
 
     def tokenize_data(self, data, max_length=512):
-        return self.tokenizer(data, padding=True, truncation=True, max_length=max_length, return_tensors='pt')
+        return self.tokenizer(data, truncation=True, return_tensors='pt')
 
     def prepare_dataset(self, X, y, test_size=0.2):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, stratify=y random_state=42)
         train_encodings = self.tokenize_data(X_train)
         test_encodings = self.tokenize_data(X_test)
         return train_encodings, y_train, test_encodings, y_test
@@ -29,11 +34,13 @@ class LLMModel:
             load_best_model_at_end=True
         )
         train_dataset = torch.utils.data.TensorDataset(train_encodings['input_ids'], train_encodings['attention_mask'], torch.tensor(train_labels))
-        
+        data_collator = DataCollatorWithPadding(tokenizer=self.tokenizer)
+
         trainer = Trainer(
             model=self.model,
             args=training_args,
-            train_dataset=train_dataset
+            train_dataset=train_dataset,
+            data_collator=data_collator
         )
         trainer.train()
 
