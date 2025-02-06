bbaf0ce12fcaca68b1f0c80d87c3f0d0d046ae7c
Henry Ashcroft <ashcrofthenry511@gmail.com>
2025-02-05 16:17:23 -0800

    add main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from Data_Preprocessing import Preprocessor
+from Model import LLMModel
+
+def main():
+    # Define dataset file paths
+    postings_path = "C:\Users\ashcr\Downloads\Fake Job Detector\postings.csv"
+    fake_postings_path = "C:\Users\ashcr\Downloads\Fake Job Detector\Fake Postings.csv"
+    industries_path = 'C:\Users\ashcr\Downloads\Fake Job Detector\industries.csv'
+    job_industries_path = 'C:\Users\ashcr\Downloads\Fake Job Detector\job_industries.csv'
+    
+    # Initialize preprocessor and prepare data
+    preprocessor = Preprocessor(postings_path, fake_postings_path, industries_path, job_industries_path)
+    X, y = preprocessor.prepare_data_for_model()
+    
+    # Initialize model
+    model = LLMModel(model_name='albert-base-v2', num_labels=2)
+    
+    # Prepare dataset for training
+    train_encodings, train_labels, test_encodings, test_labels, val_encodings, val_labels = model.prepare_dataset(X, y)
+    
+    # Train the model
+    model.train_model(train_encodings, train_labels, val_encodings, val_labels)
+    
+    # Save the fine-tuned model
+    model.save_model()
+    
+    print("Training complete. Model saved.")
+
+if __name__ == "__main__":
+    main()
